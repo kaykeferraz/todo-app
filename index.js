822f52e2c87ea8af1978fad96269e57e8e67a038
kayke ferraz de moraes <kaykedudu13@gmail.com>
2023-11-28 22:18:47 -0300

    criação - primeira view do projeto

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const exphbs = require("express-handlebars")
 const app = express()
 
 app.engine('handlebars', exphbs.engine())
-app.set('view enguine', 'handlebars')
+app.set('view engine', 'handlebars') 
 
-app.get('/', (requisicao,resposta) =>{
-    resposta.send("Olá mundo!")
+app.get('/', (requisicao, resposta) => {
+    resposta.render('home')
 })
 
-app.listen(3000, () =>{
+app.listen(3000, () => {
     console.log("Servidor rodando na porta 3000!")
 })
